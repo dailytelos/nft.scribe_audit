da202b6f6fdae98c7f13ecc1b2e6510876e75127
tlccstudio <tlccstudio@gmail.com>
2023-09-24 03:42:07 +0800

    sep 24 oracles.hpp testss

--- testRunner copy.js
+++ testRunner copy.js
@@ -10,6 +10,7 @@ const signatureProvider = new JsSignatureProvider([privateKey]);
 const rpc = new JsonRpc('http://localhost:8888', { fetch });
 const api = new Api({ rpc, signatureProvider });
 
+
 const testDir = __dirname + '/tests/';
 const testFilesData = fs.readdirSync(testDir)
                     .filter(file => path.extname(file) === '.json')
@@ -35,6 +36,30 @@ async function sendTransaction(account, actionName, actionData, actor, permissio
     });
 }
 
+function logErrorToFile(errorDesc, data) {
+    // Read the current content of the errors.json file
+    let errors = [];
+    if (fs.existsSync('errors.json')) {
+        const currentContent = fs.readFileSync('errors.json', 'utf8');
+        try {
+            errors = JSON.parse(currentContent);
+        } catch (e) {
+            console.error('Error parsing errors.json:', e);
+        }
+    }
+
+    // Push the new error with a timestamp to the array
+    errors.push({
+        timestamp: new Date().toISOString(),
+        error_desc: errorDesc,
+        ...data
+    });
+
+    // Write the updated array back to the errors.json file
+    fs.writeFileSync('errors.json', JSON.stringify(errors, null, 2));
+}
+
+
 describe('nft.scribe testing...', () => {
 
     beforeEach(done => {
@@ -45,24 +70,43 @@ describe('nft.scribe testing...', () => {
         describe(`${name} test:`, () => {
             data.forEach(test => {
                 it(test.description, async () => {
-                    if (test.function === 'sendTransaction') {
-                        const result = await sendTransaction(...test.params);
-                        if (test.shouldFail) {
-                            assert.notExists(result.transaction_id);
-                        } else {
-                            assert.exists(result.transaction_id);
+                    try {
+                        if (test.function === 'sendTransaction') {
+                            const result = await sendTransaction(...test.params);
+                            if (test.shouldFail) {
+                                logErrorToFile('Transaction should have failed but it succeeded', { result });
+                                assert.fail('Transaction should have failed but it succeeded');
+                            } else {
+                                assert.exists(result.transaction_id, 'Transaction failed but it should have succeeded');
+                            }
+                        } else if (test.function === 'get_table_rows') {
+                            const tableRows = await rpc.get_table_rows(test.params);
+                            assert.exists(tableRows.rows, 'Table is empty');
+                            const firstRow = tableRows.rows[0];
+                            for (const key in test.expectedValues) {
+                                if ("oracle" in firstRow) {
+                                    if(firstRow.oracle[key] !== test.expectedValues[key]) {
+                                        logErrorToFile("tableRows provided...", tableRows);
+                                        logErrorToFile(`Error struct_oracle: ${key} does not match`, firstRow.oracle[key]);
+                                    }
+                                    assert.equal(firstRow.oracle[key], test.expectedValues[key], `${key} does not match`);
+                                } else {
+                                    if (firstRow[key] !== test.expectedValues[key]) {
+                                        logErrorToFile(`${key} does not match`, firstRow[key]);
+                                    }
+                                    assert.equal(firstRow[key], test.expectedValues[key], `${key} does not match`);
+                                }
+                            }
                         }
-                    } else if (test.function === 'get_table_rows') {
-                        const tableRows = await rpc.get_table_rows(test.params);
-                        assert.exists(tableRows.rows, 'Table is empty');
-                        const network = tableRows.rows[0];
-                        for (const key in test.expectedValues) {
-                            assert.equal(network[key], test.expectedValues[key], `${key} does not match`);
+                    } catch (error) {
+                        if (!test.shouldFail) {
+                            //logErrorToFile(error.message, {});
+                            throw error; // If the test is not supposed to fail, throw the error
                         }
+                        // If the test is supposed to fail, and it does, then it's a success
                     }
                 });
             });
         });
     });
-    
 });
--- testRunner.js
+++ testRunner.js
@@ -10,6 +10,7 @@ const signatureProvider = new JsSignatureProvider([privateKey]);
 const rpc = new JsonRpc('http://localhost:8888', { fetch });
 const api = new Api({ rpc, signatureProvider });
 
+
 const testDir = __dirname + '/tests/';
 const testFilesData = fs.readdirSync(testDir)
                     .filter(file => path.extname(file) === '.json')
@@ -35,6 +36,30 @@ async function sendTransaction(account, actionName, actionData, actor, permissio
     });
 }
 
+function logErrorToFile(errorDesc, data) {
+    // Read the current content of the errors.json file
+    let errors = [];
+    if (fs.existsSync('errors.json')) {
+        const currentContent = fs.readFileSync('errors.json', 'utf8');
+        try {
+            errors = JSON.parse(currentContent);
+        } catch (e) {
+            console.error('Error parsing errors.json:', e);
+        }
+    }
+
+    // Push the new error with a timestamp to the array
+    errors.push({
+        timestamp: new Date().toISOString(),
+        error_desc: errorDesc,
+        ...data
+    });
+
+    // Write the updated array back to the errors.json file
+    fs.writeFileSync('errors.json', JSON.stringify(errors, null, 2));
+}
+
+
 describe('nft.scribe testing...', () => {
 
     beforeEach(done => {
@@ -49,6 +74,7 @@ describe('nft.scribe testing...', () => {
                         if (test.function === 'sendTransaction') {
                             const result = await sendTransaction(...test.params);
                             if (test.shouldFail) {
+                                logErrorToFile('Transaction should have failed but it succeeded', { result });
                                 assert.fail('Transaction should have failed but it succeeded');
                             } else {
                                 assert.exists(result.transaction_id, 'Transaction failed but it should have succeeded');
@@ -56,13 +82,25 @@ describe('nft.scribe testing...', () => {
                         } else if (test.function === 'get_table_rows') {
                             const tableRows = await rpc.get_table_rows(test.params);
                             assert.exists(tableRows.rows, 'Table is empty');
-                            const network = tableRows.rows[0];
+                            const firstRow = tableRows.rows[0];
                             for (const key in test.expectedValues) {
-                                assert.equal(network[key], test.expectedValues[key], `${key} does not match`);
+                                if ("oracle" in firstRow) {
+                                    if(firstRow.oracle[key] !== test.expectedValues[key]) {
+                                        logErrorToFile("tableRows provided...", tableRows);
+                                        logErrorToFile(`Error struct_oracle: ${key} does not match`, firstRow.oracle[key]);
+                                    }
+                                    assert.equal(firstRow.oracle[key], test.expectedValues[key], `${key} does not match`);
+                                } else {
+                                    if (firstRow[key] !== test.expectedValues[key]) {
+                                        logErrorToFile(`${key} does not match`, firstRow[key]);
+                                    }
+                                    assert.equal(firstRow[key], test.expectedValues[key], `${key} does not match`);
+                                }
                             }
                         }
                     } catch (error) {
                         if (!test.shouldFail) {
+                            //logErrorToFile(error.message, {});
                             throw error; // If the test is not supposed to fail, throw the error
                         }
                         // If the test is supposed to fail, and it does, then it's a success
@@ -71,6 +109,4 @@ describe('nft.scribe testing...', () => {
             });
         });
     });
-    
-    
 });
